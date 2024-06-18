e334bd26cef82d568ccca90e80cddbb4f52cfe90
gdd12 <vytalpvp@gmail.com>
2024-06-17 21:02:21 -0700

    Remove un-needed code. Added URL entry.

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,11 +1,35 @@
 import React, {useState} from 'react'
-import MethodSelection from './MethodSelection';
+import { methodList } from '../constants/methods';
 
 function Request() {
-  
+  const [method, setMethod] = useState("GET")
+  const [url, setUrl] = useState("")
+
+  const handleMethodChange = (e) => {
+    console.log(`Method changed to: ${e.target.value}`)
+    setMethod(e.target.value)
+  };
+
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value)
+  }
+
+  const sendRequest = () => {
+    console.log(method, url)
+  }
+
   return(
     <>
-      <MethodSelection/>
+    {/* {Selecting the method of the HTTP request} */}
+      <select value={method} onChange={handleMethodChange}>
+        {methodList.map((method,index) => (
+          <option key={index} value={method}>
+            {method}
+          </option>
+        ))}
+      </select>
+      <input value={url} onChange={e => handleUrlChange(e)} type="url" placeholder="Enter URL or paste text"/>
+      <button onClick={sendRequest}>SEND</button>
     </>
   )
 }
